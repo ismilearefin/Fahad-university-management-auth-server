86579a985a5e76eb5c412d4ad30807f6cb3989a8
ismilearefin <arefinismile@gmail.com>
2023-05-29 20:13:36 +0600

    [DONE] setup express, mongoose, cors, parser

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import app from "./app";
+import config from "./config";
+import mongoose from "mongoose";
+
+ 
+
+
+
+async function main() {
+  try{
+    await mongoose.connect(config.database_url as string);
+    console.log(`successfully connected to Database`);
+
+    app.listen(config.port, () => {
+        console.log(`Example app listening on port ${config.port}`);
+      })
+  }catch(err) {
+    console.log(`Failed to connect to Database.`,err)
+  }
+
+}
+main();
