ef9ee6fb07d77b9fb0c7580340ff64b1c81efa0f
ismilearefin <arefinismile@gmail.com>
2023-06-04 03:46:14 +0600

    Install winston , customize logs

---
+++ src/shared/logger.ts
@@ -0,0 +1,62 @@
+import {createLogger, format, transports} from 'winston';
+const { combine, timestamp, label, prettyPrint,printf } = format;
+import path from 'path';
+import  DailyRotateFile from 'winston-daily-rotate-file';
+
+
+// custom formatting
+const myFormat = printf(({ level, message, label, timestamp }) => {
+    const date = new Date(timestamp);
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    return `${date.toDateString()} ${hour}:${minutes}:${seconds} [${label}] ${level}: ${message}`;
+  });
+
+ 
+
+
+const logger = createLogger({
+  level: 'info',
+  format: combine(
+    label({ label: 'Fahad Uni' }),
+    timestamp(),
+    myFormat,
+    prettyPrint()
+  ),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+        filename: path.join(process.cwd(), 'logs','winston','success' ,'uni-%DATE%-success.log'),
+        datePattern: 'YYYY-MM-DD-HH',
+        zippedArchive: true,
+        maxSize: '20m',
+        maxFiles: '14d'
+      }),
+    
+  ],
+});
+
+const errorLogger = createLogger({
+  level: 'error',
+  format: combine(
+    label({ label: 'Fahad Uni' }),
+    timestamp(),
+    myFormat,
+    prettyPrint()
+  ),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+        filename: path.join(process.cwd(), 'logs','winston', 'error','uni-%DATE%-error.log'),
+        datePattern: 'YYYY-MM-DD-HH',
+        zippedArchive: true,
+        maxSize: '20m',
+        maxFiles: '14d'
+      }),
+  ],
+});
+
+
+
+export { logger,errorLogger };
